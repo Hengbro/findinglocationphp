67e039695100a1651e5bdd0825f6c158b2bc4dc8
Hengbro <hengkisaoremaha@gmail.com>
2023-05-01 14:40:38 +0700

    ProjectFindingLocation

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+
+        $validasi = Validator::make($request->all(), [
+            'email' => 'required',
+            'password' => 'required|min:6'
+        ]);
+
+        if ($validasi->fails()) {
+            return $this->error('Kesalahan: ' . $validasi->errors()->first());
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        if ($user) {
+            if (password_verify($request->password, $user->password)) {
+                return $this->success($user, 'Selamat datang di Finding ' . $user->name);
+            } else {
+                return $this->error('Email or Password salah');
+            }
+        }
+        return $this->error('Kesalahan: ' . $validasi->errors()->first());
+    }
+
+    public function register(Request $request){
+        $validasi = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|unique:users',
+            'phone' => 'required|unique:users',
+            'kota' => 'required',
+            'password' => 'required|min:6'
+        ]);
+
+        if ($validasi->fails()) {
+            return $this->error('Kesalahan: '.$validasi->errors()->first());
+        }
+
+        $user = User::create(array_merge($request->all(),[
+            'password' => bcrypt($request->password)
+        ]));
+
+        if ($user) {
+            return $this->success($user, 'Akun anda '. $user->name. ' berhasil terdaftar');
+        } else {
+            return $this->error('Kesalahan: '.$validasi->errors()->first());
+        }
+    }
+
+    public function update(Request $request, $id){
+        $user = User::where('id', $id)->first();
+        if ($user){
+            $user->update($request->all());
+            return $this->success($user);
+        }
+        return $this->error("Pengguna tidak di temukan");
+    }
+    public function upload(Request $request, $id){
+        $user = User::where('id', $id)->first();
+        if ($user){
+            $fileName ="";
+            if($request->image){
+                $image = $request->image->getClientOriginalName();
+                $image = str_replace(' ', '', $image);
+                $image = date('Hs') . rand(1,999) . "_" . $image;
+                $fileName = $image;
+                $request->image->storeAs('public/user', $image);
+            }else{
+                return $this->error("Image wajib di kirim");
+            }
+            $user->update([
+                'image' => $fileName
+            ]);
+            return $this->success($user);
+        }
+        return $this->error("Pengguna tidak di temukan");
+    }
+
+    public function success($user, $message = "success"){
+        return response()->json([
+            'code' => 200,
+            'message' => $message,
+            'data' =>$user
+        ]);
+    }
+
+    public function error($message){
+        return response()->json([
+            'code' => 400,
+            'message' => $message
+        ], 400);
+    }
+
+}
